bdc59251dc8e5ba35ab25ecaccf29cacdd4233b0
Krestyaninov12 <kreda@pnpe.ru>
2022-02-04 09:50:07 +0300

    Добавьте файлы проекта.

---
+++ Lab 2.14/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Task_36_WPF_14
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<Product> products;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            products = new ObservableCollection<Product>();
+            products.Add(new Product()
+            {
+                NameProduct = "Рубашка",
+                Price = 3000,                
+                Image = "/Image/1.jpg",
+                Category = Category.Одежда
+            });
+            products.Add(new Product()
+            {
+                NameProduct = "Галстук",
+                Price = 900,
+                Image = "/Image/2.jpg",
+                Category = Category.Одежда
+            });
+            products.Add(new Product()
+            {
+                NameProduct = "Телефон",
+                Price = 1200,
+                Image = "/Image/3.jpg",
+                Category = Category.Техника
+            });
+            products.Add(new Product()
+            {
+                NameProduct = "Принтер",
+                Price = 3500,
+                Image = "/image/4.jpg",
+                Category = Category.Техника
+            });
+            IstBox.ItemsSource = products;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            products.Add(new Product()
+            {
+                NameProduct = "Автомобиль",
+                Price = 3200000,
+                Image = "/Image/Avto.jpg",
+                Category = Category.Транспорт
+            });
+        }
+    }
+}
---
+++ Lab 2.14/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_36_WPF_14
+{
+    public enum Category
+    {
+        Food,
+        Одежда,
+        Техника,
+        Транспорт
+    }
+    internal class Product
+    {
+        public string NameProduct { get; set; }
+        public int Price { get; set; }
+        public string Image { get; set; }
+        public Category Category { get; set; }
+        public override string ToString()
+        {
+            return NameProduct;
+        }
+    }
+}
